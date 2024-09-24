393e093059ab3d37da12ade7a428da4c8cb6bdee
adeenajaweid96 <adeenajaweid96@gmail.com>
2024-09-24 10:49:57 -0700

    smit-js

---
+++ chap43-48/app.js
@@ -0,0 +1,38 @@
+// 1. Show an alert box on click on a link.
+
+/*function here(){
+    alert("Hello, World!");
+}*/
+
+
+// 2. Display some Mobile images in browser. On click on an
+// image Show the message in alert to user.
+
+// function display(){
+// alert("Thanks for purchasing a phone from us");
+// }
+
+// 3. Display 10 student records in table and each row should contain a delete
+// button. If you click on a button to delete a record, entire row should be
+// deleted.
+
+// function del(e){
+// e.parentNode.parentNode.remove();
+// }
+
+
+
+// 5. Show a counter in browser. Counter should increase on click on increase
+// button and decrease on click on decrease button. And show updated counter
+// value in browser.
+
+/*var counter = 0;
+function add(){
+    counter++;
+    document.getElementById("counter").innerHTML = counter;
+}
+function subtract(){
+    counter--;
+    document.getElementById("counter").innerHTML = counter;
+
+}*/
